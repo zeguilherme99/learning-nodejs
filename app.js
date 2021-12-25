94d3d8af4800dd466971c9e13ff73a1300462314
zeguilherme99 <zeguilherme008@hotmail.com>
2021-12-24 23:33:33 -0300

    Lesson 01

--- app.js
+++ app.js
@@ -1,22 +1,14 @@
-const http  = require('http');
-
 const express = require('express');
 
-const app = express()
+const app = express();
 
-app.use('/', (req, res, next) => {
-    console.log('This always runs');
-    next();
-});
-
-app.use('/add-product', (req, res, next) => {
-    console.log('In the middleware');
-    res.send('<h1>The "Add product" page</h1>')
-});
-
-app.use('/', (req, res, next) => {
+app.use('/users', (req, res, next) => {
     console.log('In another middleware');
-    res.send('<h1>Hello form Express JS</h1>')
+    res.send('<h1>Users</h1>')
+});
+
+app.use('/', (req, res, next) => {
+    res.send('<h1>Learning JS</h1>')
 });
 
 app.listen(3000);
