eb73bb29c67699f983c86b20732427678dc68e3a
zeguilherme99 <zeguilherme008@hotmail.com>
2021-12-25 18:10:36 -0300

    Helper function for Navigation

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path')
+const rootDir = require('./util/path')
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
@@ -14,7 +15,7 @@ app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-    res.status(404).sendFile(path.join(__dirname, '/views', '404.html'));
+    res.status(404).sendFile(path.join(rootDir, '/views', '404.html'));
 })
 
 app.listen(3000);
