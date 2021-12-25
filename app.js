0f4d2bba48995a6395adab24d59e5f6d565f39c4
zeguilherme99 <zeguilherme008@hotmail.com>
2021-12-24 23:57:51 -0300

    Learning about bodyParser

--- app.js
+++ app.js
@@ -1,21 +1,21 @@
-const http  = require('http');
-
 const express = require('express');
+const bodyParser = require('body-parser')
 
 const app = express()
 
-app.use('/', (req, res, next) => {
-    console.log('This always runs');
-    next();
-});
+app.use(bodyParser.urlencoded({extended: false}))
 
 app.use('/add-product', (req, res, next) => {
     console.log('In the middleware');
-    res.send('<h1>The "Add product" page</h1>')
+    res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>')
+});
+
+app.post('/product', (req, res, next) => {
+    console.log(req.body);
+    res.redirect('/');
 });
 
 app.use('/', (req, res, next) => {
-    console.log('In another middleware');
     res.send('<h1>Hello form Express JS</h1>')
 });
 
