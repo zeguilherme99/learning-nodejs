4b3851792c9f6d14e1b5d0998454f3b7ba2b5b34
zeguilherme99 <zeguilherme008@hotmail.com>
2021-12-30 20:59:30 -0300

    Add Controllers

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
 
-const adminData = require('./routes/admin');
+const errorController = require('./controllers/error');
+
+const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
 const app = express();
@@ -13,11 +15,9 @@ app.set('views', 'views'); // onde achar os templates
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public'))) //acha a pasta public dentro do projeto
 
-app.use('/admin', adminData.routes); // uma forma de importar as rotas
+app.use('/admin', adminRoutes.routes); // uma forma de importar as rotas
 app.use(shopRoutes); // outra forma de se importar as rotas
 
-app.use((req, res, next) => {
-    res.status(404).render("404", {pageTitle: 'Page Not Found'});
-})
+app.use(errorController.get404);
 
 app.listen(3000);
--- routes/shop.js
+++ routes/shop.js
@@ -1,22 +1,11 @@
-const path = require('path');
-const rootDir = require('../util/path');
-
 const express = require('express');
 
-const adminData = require('./admin');
+const path = require('path');
+
+const productsController = require('../controllers/products')
 
 const router = express.Router();
 
-router.get('/', (req, res, next) => {
-    const products = adminData.products;
-    res.render('shop',{ 
-        prods: products,
-        path: '/',
-        pageTitle: 'Shop',
-        hasProducts: products.length > 0,
-        activeShop: true,
-        productCss: true
-    }); // objetos que vão para dentro do pug ou handlebars
-});
+router.get('/', productsController.getProducts);
 
 module.exports = router; // uma forma de exportar as rotas
