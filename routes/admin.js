c6d848f32970ac601b591b169dc84152447b85b6
zeguilherme99 <zeguilherme008@hotmail.com>
2021-12-30 18:29:55 -0300

    Handlebars Pages

--- routes/admin.js
+++ routes/admin.js
@@ -9,7 +9,13 @@ const products = [];
 
 // /admin/add-product => GET
 router.get('/add-product', (req, res, next) => {
-    res.render('add-product',{ pageTitle: 'Add Product', path: '/admin/add-product' });
+    res.render('add-product',{
+        pageTitle: 'Add Product',
+        path: '/admin/add-product',
+        activeAddProduct: true,
+        formsCSS: true,
+        productCSS: true
+    });
 });
 
 // /admin/add-product => POST
--- routes/shop.js
+++ routes/shop.js
@@ -9,7 +9,13 @@ const router = express.Router();
 
 router.get('/', (req, res, next) => {
     const products = adminData.products;
-    res.render('shop', { prods: products,path: '/', pageTitle: 'Shop', hasProducts: products.length > 0 }); // objetos que vão para dentro do pug ou handlebars
+    res.render('shop',{ 
+        prods: products,path: '/',
+        pageTitle: 'Shop',
+        hasProducts: products.length > 0,
+        activeShop: true,
+        productCss: true
+    }); // objetos que vão para dentro do pug ou handlebars
 });
 
 module.exports = router; // uma forma de exportar as rotas
