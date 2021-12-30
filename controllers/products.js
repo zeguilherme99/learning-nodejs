0ffb721382161604aa16ffe6860ea72ddc4ba19c
zeguilherme99 <zeguilherme008@hotmail.com>
2021-12-30 20:59:00 -0300

    Add Controllers

---
+++ controllers/products.js
@@ -0,0 +1,27 @@
+const products = [];
+
+exports.getAddProduct = (req, res, next) => {
+    res.render('add-product',{
+        pageTitle: 'Add Product',
+        path: '/admin/add-product',
+        activeAddProduct: true,
+        formsCSS: true,
+        productCSS: true
+    });
+}
+
+exports.postAddProduct = (req, res, next) => {
+    products.push({ title: req.body.title });
+    res.redirect('/');
+}
+
+exports.getProducts = (req, res, next) => {
+    res.render('shop',{ 
+        prods: products,
+        path: '/',
+        pageTitle: 'Shop',
+        hasProducts: products.length > 0,
+        activeShop: true,
+        productCss: true
+    }); // objetos que vão para dentro do pug ou handlebars
+}
