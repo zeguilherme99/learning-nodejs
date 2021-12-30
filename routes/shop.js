b66f40ce140b33033ef6a28f076da4daa09c0fd5
zeguilherme99 <zeguilherme008@hotmail.com>
2021-12-30 20:37:02 -0300

    Adjustando

--- routes/shop.js
+++ routes/shop.js
@@ -10,7 +10,8 @@ const router = express.Router();
 router.get('/', (req, res, next) => {
     const products = adminData.products;
     res.render('shop',{ 
-        prods: products,path: '/',
+        prods: products,
+        path: '/',
         pageTitle: 'Shop',
         hasProducts: products.length > 0,
         activeShop: true,
