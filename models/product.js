bc8b2a16b7d0862079ad69e60b9a7f029686917f
zeguilherme99 <zeguilherme008@hotmail.com>
2021-12-30 22:11:37 -0300

    Refacttoring the File Storage Code

--- models/product.js
+++ models/product.js
@@ -1,6 +1,18 @@
 const fs = require('fs');
 const path = require('path');
-const rootDir = require('../util/path')
+const rootDir = require('../util/path');
+
+const p = path.join(rootDir, 'data', 'products.json');
+
+const getProductFromFile = (cb) => {
+    fs.readFile(p, (err, fileContent) => {
+        if (err) {
+            return cb([]);
+        } else {
+            cb(JSON.parse(fileContent));
+        }
+    });
+}
 
 module.exports = class Product {
     constructor(t) {
@@ -8,12 +20,7 @@ module.exports = class Product {
     }
 
     save() {
-        const p = path.join(rootDir, 'data', 'products.json');
-        fs.readFile(p, (err, fileContent) => {
-            let products = [];
-            if (!err) {
-                products = JSON.parse(fileContent);
-            }
+        getProductFromFile(products => {
             products.push(this);
             fs.writeFile(p, JSON.stringify(products), (err) => {
                 console.log(err);
@@ -22,12 +29,6 @@ module.exports = class Product {
     }
 
     static fetchAll(cb) {
-        const p = path.join(rootDir, 'data', 'products.json');
-        fs.readFile(p, (err, fileContent) => {
-            if (err) {
-                cb([]);
-            }
-            cb(JSON.parse(fileContent));
-        })
+        getProductFromFile(cb);
     };
 }
