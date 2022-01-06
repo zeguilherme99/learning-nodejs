08fe6ae583e061d25901d225b74244c0ac6bfcd7
zeguilherme99 <zeguilherme008@hotmail.com>
2022-01-05 21:21:40 -0300

    Using Query Params

--- routes/admin.js
+++ routes/admin.js
@@ -14,6 +14,8 @@ router.get('/products', adminController.getProducts);
 // /admin/add-product => POST
 router.post('/add-product', adminController.postAddProduct);
 
+router.get('/edit-product/:productId', adminController.getEditProduct);
+
 
 exports.routes = router; // outra forma de se importar as rotas
 
